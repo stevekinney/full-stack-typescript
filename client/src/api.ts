6056d4b283b3b99c4848e5f58e7d1b2777083651
Steve Kinney <hello@stevekinney.net>
2025-03-20 02:35:28 -0500

    Remove `await` and comments

--- client/src/api.ts
+++ client/src/api.ts
@@ -2,7 +2,6 @@ import { PartialTask, Task } from './types';
 
 const API_URL = 'http://localhost:4001';
 
-// Get all tasks
 export const fetchTasks = async (showCompleted: boolean): Promise<Task[]> => {
   const url = new URL(`/tasks`, API_URL);
 
@@ -16,7 +15,7 @@ export const fetchTasks = async (showCompleted: boolean): Promise<Task[]> => {
     throw new Error('Failed to fetch tasks');
   }
 
-  return await response.json();
+  return response.json();
 };
 
 export const getTask = async (id: string): Promise<Task> => {
@@ -27,10 +26,9 @@ export const getTask = async (id: string): Promise<Task> => {
     throw new Error('Failed to fetch task');
   }
 
-  return (await response.json()) as Task;
+  return response.json();
 };
 
-// Create a new task
 export const createTask = async (task: PartialTask): Promise<void> => {
   const url = new URL('/tasks', API_URL);
 
@@ -47,7 +45,6 @@ export const createTask = async (task: PartialTask): Promise<void> => {
   }
 };
 
-// Update a task
 export const updateTask = async (id: string, task: PartialTask): Promise<void> => {
   const url = new URL(`/tasks/${id}`, API_URL);
 
@@ -64,7 +61,6 @@ export const updateTask = async (id: string, task: PartialTask): Promise<void> =
   }
 };
 
-// Delete a task
 export const deleteTask = async (id: string): Promise<void> => {
   const url = new URL(`/tasks/${id}`, API_URL);
 
