92fbe3d2998d139ca03db7b7d3ab5d806a493628
Steve Kinney <hello@stevekinney.net>
2025-03-19 20:20:36 -0500

    Update schemas.ts

--- shared/schemas.ts
+++ shared/schemas.ts
@@ -1,18 +1,3 @@
 import { z } from 'zod';
 
-export const TaskSchema = z.object({
-  id: z.coerce.number().int(),
-  title: z.string(),
-  description: z.string().optional(),
-  completed: z.coerce.boolean(),
-});
-
-export const TaskParamsSchema = TaskSchema.pick({ id: true });
-export const TaskQuerySchema = TaskSchema.pick({ completed: true }).partial();
-
-export const TasksSchema = z.array(TaskSchema);
-
-export const NewTaskSchema = TaskSchema.omit({ id: true, completed: true });
-export const UpdateTaskSchema = TaskSchema.partial().omit({ id: true });
-
 export {};
